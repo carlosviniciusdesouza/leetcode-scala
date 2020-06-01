5acb47d88c8657bca89550539a836f82b7a2b480
Carlos Souza <viniciusbr93@gmail.com>
2020-06-01 18:55:45 +0200

    First Problem

---
+++ src/solutions/TwoSum.scala
@@ -0,0 +1,14 @@
+package solutions
+
+object TwoSum {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var a :  Option[Array[Int]] = Option.empty
+    for(i <- 0 to (nums.length - 1) ; j <- 0 to (nums.length - 1) if (i != j ))
+      if (nums(i) + nums(j) == target) {a = Option(Array(j,i)) }
+    a.get
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(twoSum(Array(2,7,11,15), 9).toList)
+  }
+}
